2ac44f33677ad1181df673d0aa846c5e5f069dca
unknown <mh.guo@JXSX02OF1487.luxshare.com.cn>
2026-08-05 18:10:49 +0800

    API允许跨域访问

--- JXSX_COD/ASP_API/APItest/APItest/Controllers/HomeController.cs
+++ JXSX_COD/ASP_API/APItest/APItest/Controllers/HomeController.cs
@@ -7,18 +7,32 @@ using System.Web.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Net.Http.Headers;
 namespace APItest.Controllers
 {
 
     public class HomeController : ApiController
     {
         [HttpGet]
-        public string index()
+        public HttpResponseMessage index()
         {
             int a = 3;
             int b = 4;
             string c = (a + b) + "aaaa";
-            return c;
+
+            var body = new { msg = c };
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, body);
+
+            // 设置自定义头
+            //response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:5173");   //表示只允许http://localhost:5173跨域访问
+            response.Headers.Add("Access-Control-Allow-Origin", "*");   //表示允许所有人跨域访问
+
+            // content头部
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return response;
+
+            //return c;
         }
 
 
