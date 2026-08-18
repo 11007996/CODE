6dc450c30b802cc62d5343ac31c860e5de349f7c
unknown <mh.guo@JXSX02OF1487.luxshare.com.cn>
2026-08-18 17:05:05 +0800

    SSE

---
+++ JXSX_COD/C#网络编程/SSEServer/SSEClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+
+namespace SSEClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string url = "http://localhost:8080/sse_endpoint"; // SSE endpoint URL
+            string url = "http://localhost:8080"; // SSE endpoint URL
+            sseclient(url);
+            Console.ReadKey();
+            
+        }
+        static async Task sseclient(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                request.Headers.Add("Accept", "text/event-stream");
+
+                using (HttpResponseMessage response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                    using (var reader = new System.IO.StreamReader(responseStream))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            string line = await reader.ReadLineAsync();
+
+                            if (!string.IsNullOrWhiteSpace(line))
+                            {
+                                Console.WriteLine(line);
+                                // Handle the received SSE event here
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ JXSX_COD/C#网络编程/SSEServer/SSEServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SSEServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+
+            Console.WriteLine("Server is running...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                ThreadPool.QueueUserWorkItem((_) =>
+                {
+                    SendEvent(context);
+                });
+            }
+        }
+            static void SendEvent(HttpListenerContext context)
+        {
+            try
+            {
+                context.Response.ContentType = "text/event-stream";
+                context.Response.StatusCode = 200;
+                context.Response.Headers.Add("Cache-Control", "no-cache");
+                context.Response.Headers.Add("Connection", "keep-alive");
+  
+                while (true)
+                {
+                    string message = string.Format("data: {0}\n\n",DateTime.Now.ToString("HH:mm:ss"));
+                    byte[] bytes = Encoding.UTF8.GetBytes(message);
+                    context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+                    context.Response.OutputStream.Flush();
+                    Thread.Sleep(1000); // 每秒发送一次消息
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                context.Response.Close();
+            }
+        }
+    }
+}
